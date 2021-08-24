2e37b338d8e8131fddef05c51d6619dc94a62ee1
Udhay <udhay1co.de@gmail.com>
2021-08-24 14:12:30 +0530

    working around shapes and stuff

--- gamev1.py
+++ gamev1.py
@@ -1,5 +1,12 @@
 from ursina import *
 
+# Creating a variable for the engine
 app = Ursina()
+
+
+#creating basic objects
+test_square = Entity(model = 'quad' , color = color.red , scale = (1 , 4))
+
+#running the engine (creates a window )
 
 app.run()
